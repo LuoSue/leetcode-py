df6569de86fc2c09fdd9c2a7ae34ff11b5f06af2
LuoSue <hur73426@gmail.com>
2025-06-06 16:31:16 +0800

    modify 034

--- code/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ code/034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -3,7 +3,7 @@
 """
 @File    :   034_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
 @Time    :   2025/04/16 11:24:16
-@Author  :   rj 
+@Author  :   rj
 @Version :   1.0
 @Desc    :   在排序数组中查找元素的第一个和最后一个位置
 """
@@ -18,54 +18,58 @@ class Solution:
         如果target不存在，返回[-1, -1]
         时间复杂度: O(log n)
         """
-        
+
         def find_left(nums, target):
             """
             使用二分查找找到target的最左位置
-            :param nums: 排序数组
-            :param target: 要查找的目标值
-            :return: target的最左索引，如果不存在返回-1
             """
-            left, right = -1, len(nums)
-            res = -1  # 初始化结果为-1，表示未找到
+            if not nums:
+                return -1
 
-            while left <= right:
-                mid = (left + right) // 2  # 计算中间位置
+            left, right = 0, len(nums)
+            result = -1
 
-                if nums[mid] >= target:
-                    right = mid  # 向左半部分继续搜索
-                    if nums[mid] == target:
-                        res = mid  # 更新结果为当前找到的target位置
+            while left < right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    result = mid  # 找到目标，但继续向左搜索
+                    right = mid
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    left = mid  # 向右半部分继续搜索
+                    right = mid
 
-            return res
+            return result
 
         def find_right(nums, target):
             """
             使用二分查找找到target的最右位置
-            :param nums: 排序数组
-            :param target: 要查找的目标值
-            :return: target的最右索引，如果不存在返回-1
             """
-            left, right = -1, len(nums)
-            res = -1  # 初始化结果为-1，表示未找到
+            if not nums:
+                return -1
 
-            while left <= right:
-                mid = (left + right) // 2  # 计算中间位置
+            left, right = 0, len(nums)
+            result = -1
 
-                if nums[mid] <= target:
-                    left = mid  # 向右半部分继续搜索
-                    if nums[mid] == target:
-                        res = mid  # 更新结果为当前找到的target位置
+            while left < right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    result = mid  # 找到目标，但继续向右搜索
+                    left = mid + 1
+                elif nums[mid] < target:
+                    left = mid + 1
                 else:
-                    right = mid  # 向左半部分继续搜索
+                    right = mid
 
-            return res
+            return result
 
-        # 如果最左位置没找到，说明target不存在于数组中
-        if find_left(nums, target) == -1:
+        # 先找左边界
+        left_pos = find_left(nums, target)
+        if left_pos == -1:
             return [-1, -1]
 
-        # 返回最左和最右位置
-        return [find_left(nums, target), find_right(nums, target)]
+        # 再找右边界
+        right_pos = find_right(nums, target)
+        return [left_pos, right_pos]
