e61e43a30e107daa70eb63db61240e12b2e60e5f
LuoSue <hur73426@gmail.com>
2025-07-10 15:34:36 +0800

    modify 151

--- code/151_Reverse_Words_in_a_String.py
+++ code/151_Reverse_Words_in_a_String.py
@@ -16,16 +16,17 @@ class Solution:
         # 反转单词列表
         words.reverse()
         # 用单个空格连接单词列表
-        return ' '.join(words)
-    
-    
+        return " ".join(words)
+
     def reverseWords(self, s: str) -> str:
-        s = s.strip()                            # 删除首尾空格
+        s = s.strip()  # 删除首尾空格
         i = j = len(s) - 1
         res = []
         while i >= 0:
-            while i >= 0 and s[i] != ' ': i -= 1 # 搜索首个空格
-            res.append(s[i + 1: j + 1])          # 添加单词
-            while i >= 0 and s[i] == ' ': i -= 1 # 跳过单词间空格
-            j = i                                # j 指向下个单词的尾字符
-        return ' '.join(res)                     # 拼接并返回
+            while i >= 0 and s[i] != " ":
+                i -= 1  # 搜索首个空格
+            res.append(s[i + 1 : j + 1])  # 添加单词
+            while i >= 0 and s[i] == " ":
+                i -= 1  # 跳过单词间空格
+            j = i  # j 指向下个单词的尾字符
+        return " ".join(res)  # 拼接并返回
